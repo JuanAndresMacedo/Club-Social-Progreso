20288967467f8d97dfb5d60f0d321fc11d306b01
JuanAndresMacedo <juanmacedo2003@hotmail.com>
2024-08-18 22:14:22 -0300

    Cambios en ruta de noticias

--- src/app/news/news-routing.module.ts
+++ src/app/news/news-routing.module.ts
@@ -3,7 +3,8 @@ import { RouterModule, Routes } from '@angular/router';
 import { NewsDetailComponent } from './news-detail/news-detail.component';
 
 const routes: Routes = [
-  { path: 'news/:id',
+  {
+    path: 'noticia/:id',
     component: NewsDetailComponent
   },
 ];
